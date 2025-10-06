4dd23b1440a2d1ad0de0ec5fd5b3b9d40dd34322
issi <issi@gmail.com>
2025-10-07 05:22:51 +0900

    test run-vscode.bat

--- run-vscode.bat
+++ run-vscode.bat
@@ -1,17 +1,27 @@
 
 @echo off
+setlocal enabledelayedexpansion
 
 if not exist "%~dp0potable-cmd.bat" (
     echo potable-cmd.bat not found. Downloading...
-    curl -L -o "%~dp0potable-cmd.bat" "https://github.com/cotab-org/cotab/raw/refs/heads/main/potable-cmd.bat"
+    curl -L -o "%~dp0potable-cmd.bat" "https://github.com/cotab-org/cotab/raw/refs/heads/main/potable-cmd.bat" --crlf
     if ERRORLEVEL 1 goto :ERROR
 )
 call "%~dp0potable-cmd.bat"
 if ERRORLEVEL 1 goto :ERROR
 
-goto :RUN_VSCODE
-if ERRORLEVEL 1 goto :ERROR
-exit /b 0
+echo on
+for /f "delims=" %%I in ('where code 2^>NUL') do (
+    for %%I in ("%%~fI") do set "CODE_BIN_DIR=%%~dpI"
+    for %%I in ("!CODE_BIN_DIR!..") do set "VSCODE_EXE=%%~fI\Code.exe"
+    if exist "!VSCODE_EXE!" (
+        start "" "!VSCODE_EXE!" "%~dp0"
+    ) else (
+        start code "%~dp0"
+    )
+    exit /b 0
+)
+echo 'code' command not found on PATH.
 
 :ERROR
 	echo ###################
@@ -19,22 +29,3 @@ exit /b 0
 	echo ###################
 	pause
 exit /b 1
-
-
-:RUN_VSCODE
-    for /f "delims=" %%I in ('where code 2^>NUL') do (
-        call :RUN_VSCODE_INTERNAL "%%~fI"
-        exit /b 0
-    )
-    echo 'code' command not found on PATH.
-exit /b 1
-
-:RUN_VSCODE_INTERNAL
-    for %%I in ("%~1") do set "CODE_BIN_DIR=%%~dpI"
-    for %%I in ("%CODE_BIN_DIR%..") do set "VSCODE_EXE=%%~fI\Code.exe"
-    if exist "%VSCODE_EXE%" (
-        start "" "%VSCODE_EXE%" "%~dp0"
-    ) else (
-        start code "%~dp0"
-    )
-exit /b
