7958ba2c1779ef2ec2b6ab221f361ce4ee9eea18
issi <issi@gmail.com>
2025-10-07 05:33:04 +0900

    test run-vscode.bat

--- run-vscode.bat
+++ run-vscode.bat
@@ -1,27 +1,32 @@
 
 @echo off
-setlocal enabledelayedexpansion
 
+::###################################################################################
+:: activate environment
+::###################################################################################
+
+:: check and download potable-cmd.bat
 if not exist "%~dp0potable-cmd.bat" (
     echo potable-cmd.bat not found. Downloading...
-    curl -L -o "%~dp0potable-cmd.bat" "https://github.com/cotab-org/cotab/raw/refs/heads/main/potable-cmd.bat" --crlf
+    curl -L -o "%~dp0potable-cmd.bat" "https://github.com/cotab-org/cotab/raw/refs/heads/main/potable-cmd.bat"
     if ERRORLEVEL 1 goto :ERROR
 )
+
+:: activate environment
 call "%~dp0potable-cmd.bat"
 if ERRORLEVEL 1 goto :ERROR
 
-echo on
-for /f "delims=" %%I in ('where code 2^>NUL') do (
-    for %%I in ("%%~fI") do set "CODE_BIN_DIR=%%~dpI"
-    for %%I in ("!CODE_BIN_DIR!..") do set "VSCODE_EXE=%%~fI\Code.exe"
-    if exist "!VSCODE_EXE!" (
-        start "" "!VSCODE_EXE!" "%~dp0"
-    ) else (
-        start code "%~dp0"
-    )
-    exit /b 0
-)
-echo 'code' command not found on PATH.
+::###################################################################################
+:: main
+::###################################################################################
+
+goto :RUN_VSCODE
+if ERRORLEVEL 1 goto :ERROR
+exit /b 0
+
+::###################################################################################
+:: functions
+::###################################################################################
 
 :ERROR
 	echo ###################
@@ -29,3 +34,22 @@ echo 'code' command not found on PATH.
 	echo ###################
 	pause
 exit /b 1
+
+:RUN_VSCODE
+    :: search code command and start vscode
+    for /f "delims=" %%I in ('where code 2^>NUL') do (
+        call :RUN_VSCODE_INTERNAL "%%~fI"
+        exit /b 0
+    )
+    echo 'code' command not found on PATH.
+exit /b 1
+
+:RUN_VSCODE_INTERNAL
+    for %%I in ("%~1") do set "CODE_BIN_DIR=%%~dpI"
+    for %%I in ("%CODE_BIN_DIR%..") do set "VSCODE_EXE=%%~fI\Code.exe"
+    if exist "%VSCODE_EXE%" (
+        start "" "%VSCODE_EXE%" "%~dp0"
+    ) else (
+        start code "%~dp0"
+    )
+exit /b 0
